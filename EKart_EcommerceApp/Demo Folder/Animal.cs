b7768184b8c99e41a4c6508d71eaf31728bc5b1d
Priyadharshini P L <priyadharshini.p-l@capgemini.com>
2024-12-21 17:46:39 +0530

    add animal file to remote priya

---
+++ EKart_EcommerceApp/Demo Folder/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace EKart_EcommerceApp.Demo_Folder
+{
+    public class Animal
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Animal");
+        }
+    }
+}
